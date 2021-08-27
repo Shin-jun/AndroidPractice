af5200c1b6b116c940dae43cabbda07be0930fe7
Shin-jun <inside1451@naver.com>
2021-08-28 00:20:28 +0900

    digital

---
+++ DigitClassifier/app/src/main/java/com/shin/digitalclassifier/MyActivity.java
@@ -0,0 +1,51 @@
+package com.shin.digitalclassifier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Log.d("MyActivity", "onCreate");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MyActivity", "onDestroy");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MyActivity", "onStart");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MyActivity", "onStop");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MyActivity", "onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MyActivity", "onPause");
+
+    }
+}
