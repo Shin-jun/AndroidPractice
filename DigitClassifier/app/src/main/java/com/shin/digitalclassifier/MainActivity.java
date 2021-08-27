da4eee3e10d6bb43b631bcdbf189dbddae5ff7dd
Shin-jun <inside1451@naver.com>
2021-08-28 01:00:34 +0900

    UI

--- DigitClassifier/app/src/main/java/com/shin/digitalclassifier/MainActivity.java
+++ DigitClassifier/app/src/main/java/com/shin/digitalclassifier/MainActivity.java
@@ -3,13 +3,52 @@ package com.shin.digitalclassifier;
 import
         androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.input_Et);
+        Button button = (Button) findViewById(R.id.input_Btn);
+        TextView textView = (TextView) findViewById(R.id.output_Tv);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                textView.setText(text);
+            }
+        });
+
+        Button actBn = findViewById(R.id.activity_launch_Btn);
+        actBn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button finishBtn = findViewById(R.id.finish_Btn);
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+        });
+
     }
+
+
 }
