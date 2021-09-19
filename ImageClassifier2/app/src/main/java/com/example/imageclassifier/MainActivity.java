65fb61c1d2d5db83e26a5d681dfcaf44f634a06b
Shin-jun <inside1451@naver.com>
2021-09-19 23:53:29 +0900

    .

--- ImageClassifier2/app/src/main/java/com/example/imageclassifier/MainActivity.java
+++ ImageClassifier2/app/src/main/java/com/example/imageclassifier/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.imageclassifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CAMERA_PERMMISSION = Manifest.permission.CAMERA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
